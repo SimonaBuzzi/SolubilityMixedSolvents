7a453e6c86165f6664f30983580c24e86d9e3d40
Simona Buzzi <simona.buzzi@kuleuven.be>
2026-05-28 15:20:08 +0200

    fastsolv test script

---
+++ scripts_paper/fastsolv_test.py
@@ -0,0 +1,169 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+from pathlib import Path
+from rdkit.Chem import MolFromSmiles
+from fastprop.descriptors import get_descriptors
+from fastprop.defaults import ALL_2D
+from fastprop.io import load_saved_descriptors
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from scipy.stats import pearsonr
+from pytorch_lightning import Trainer
+from fastprop.data import fastpropDataLoader
+from classes import fastpropSolubility, SolubilityDataset
+
+
+RANDOM_SEED = 1701  # the final frontier
+SOLUTE_COLUMNS: list[str] = ["solute_" + d for d in ALL_2D]
+SOLVENT_COLUMNS: list[str] = ["solvent_" + d for d in ALL_2D]
+
+
+def parity_plot(truth, prediction, title, out_fpath, stat_str):
+    plt.clf()
+    plt.scatter(truth, prediction, alpha=0.1)
+    plt.xlabel("truth")
+    plt.ylabel("prediction")
+    min_val = min(np.min(truth), np.min(prediction)) - 0.5
+    max_val = max(np.max(truth), np.max(prediction)) + 0.5
+    plt.plot([min_val, max_val], [min_val, max_val], color="black", linestyle="-")
+    plt.plot(
+        [min_val, max_val],
+        [min_val + 1, max_val + 1],
+        color="red",
+        linestyle="--",
+        alpha=0.25,
+    )
+    plt.plot(
+        [min_val, max_val],
+        [min_val - 1, max_val - 1],
+        color="red",
+        linestyle="--",
+        alpha=0.25,
+    )
+    plt.ylim(min_val, max_val)
+    plt.xlim(min_val, max_val)
+    plt.text(
+        min_val,
+        max_val - 0.1,
+        stat_str,
+        horizontalalignment="left",
+        verticalalignment="top",
+    )
+    plt.title(title)
+    plt.savefig(out_fpath)
+    print("wrote plot to", out_fpath)
+    plt.show()
+    
+def test_ensemble(checkpoint_dir: Path):
+    _output_dir = checkpoint_dir.parents[0]
+    # reload the models as an ensemble
+    all_models = []
+    for checkpoint in os.listdir(checkpoint_dir):
+        model = fastpropSolubility.load_from_checkpoint(checkpoint_dir / checkpoint)
+        all_models.append(model)
+    rmses = []
+    for holdout_fpath in (
+        Path("/your/path/to/descriptors/descriptors.csv"),
+        # Path("boobier/leeds_benzene_fastprop.csv"),
+        # Path("boobier/leeds_ethanol_fastprop.csv"),
+        # Path("vermeire/solprop_fastprop_nonaq.csv"),
+    ):
+        # load the holdout data
+        df = pd.read_csv(holdout_fpath)
+        solubilities = torch.tensor(df["logS"].to_numpy(), dtype=torch.float32).unsqueeze(-1)
+        temperatures = torch.tensor(df["temperature"].to_numpy(), dtype=torch.float32).unsqueeze(-1)
+        solute_features = torch.tensor(df[SOLUTE_COLUMNS].to_numpy(), dtype=torch.float32)
+        solvent_features = torch.tensor(df[SOLVENT_COLUMNS].to_numpy(), dtype=torch.float32)
+        smiles = df[["solvent_smiles", "solute_smiles"]].apply(lambda row: ".".join(row), axis=1).tolist()
+        dataloader = fastpropDataLoader(
+            SolubilityDataset(
+                solute_features,
+                solvent_features,
+                temperatures,
+                solubilities,
+                torch.zeros_like(solubilities),
+            ),
+            batch_size=len(solubilities),
+        )
+        # run inference
+        # axis: contents
+        # 0: smiles
+        # 1: predictions
+        # 2: per-model
+        trainer = Trainer(logger=False, enable_progress_bar=False)
+        all_predictions = np.stack([torch.vstack(trainer.predict(model, dataloader)).numpy(force=True) for model in all_models], axis=2)
+        perf = np.mean(all_predictions, axis=2)
+        err = np.std(all_predictions, axis=2)
+        # interleave the columns of these arrays, thanks stackoverflow.com/a/75519265
+        res = np.empty((len(perf), perf.shape[1] * 2), dtype=perf.dtype)
+        res[:, 0::2] = perf
+        res[:, 1::2] = err
+        out = pd.DataFrame(res, columns=["logS_pred", "stdev"], index=smiles)
+        out.index.name = "smiles"
+        out.insert(0, "logS_true", df["logS"].tolist())
+        out.insert(1, "temperature", df["temperature"].tolist())
+        out.to_csv(_output_dir / (holdout_fpath.stem + "_predictions.csv"))
+
+
+        mask = ~np.isnan(out["logS_true"]) & ~np.isnan(out["logS_pred"])
+        y_true = out["logS_true"][mask]
+        y_pred = out["logS_pred"][mask]
+        
+
+        mask = ~np.isnan(out["logS_true"]) & ~np.isnan(out["logS_pred"])
+        y_true = out["logS_true"][mask]
+        y_pred = out["logS_pred"][mask]
+
+      
+        r, _ = pearsonr(y_true, y_pred)
+        mse = mean_squared_error(y_true, y_pred)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_true, y_pred)
+        wn_07 = np.count_nonzero(np.abs(y_true - y_pred) <= 0.7) / len(y_pred)
+        wn_1 = np.count_nonzero(np.abs(y_true - y_pred) <= 1.0) / len(y_pred)
+
+        stat_str = (
+            f" - Pearson's r: {r:.4f}\n"
+            f" - MAE: {mae:.4f}\n"
+            f" - MSE: {mse:.4f}\n"
+            f" - RMSE: {rmse:.4f}\n"
+            f" - W/n 0.7: {wn_07:.4f}\n"
+            f" - W/n 1.0: {wn_1:.4f}"
+        )
+
+        parity_plot(
+            y_true, 
+            y_pred, 
+            holdout_fpath.stem, 
+            _output_dir / f"{holdout_fpath.stem}_parity.png", 
+            stat_str
+        )
+
+        rmses.append(rmse)
+
+        r, _ = pearsonr(out["logS_true"], out["logS_pred"])
+        mse = mean_squared_error(out["logS_true"], out["logS_pred"])
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(out["logS_true"], out["logS_pred"])
+        wn_07 = np.count_nonzero(np.abs(out["logS_true"] - out["logS_pred"]) <= 0.7) / len(out["logS_pred"])
+        wn_1 = np.count_nonzero(np.abs(out["logS_true"] - out["logS_pred"]) <= 1.0) / len(out["logS_pred"])
+        stat_str = (
+            f" - Pearson's r: {r:.4f}\n - MAE: {mae:.4f}\n - MSE: {mse:.4f}\n - RMSE: {rmse:.4f}\n - W/n 0.7: {wn_07:.4f}\n - W/n 1.0: {wn_1:.4f}"
+        )
+        parity_plot(out["logS_true"], out["logS_pred"], holdout_fpath.stem, _output_dir / f"{holdout_fpath.stem}_parity.png", stat_str)
+        rmses.append(rmse)
+    return rmses
+
+
+if __name__ == "__main__":
+    # single runs
+    # test_ensemble(Path("output/test/checkpoints"))
+    # aleatoric error study
+    fastpropMixresults = []
+    for training_count in [226483]: 
+        # predictions = test_ensemble(Path(f"scaffold_random_correct/fastprop_{training_count}/checkpoints"))
+        predictions = test_ensemble(Path(f"output_weight1/fastprop_{training_count}/checkpoints/"))
+        fastpropMixresults.append([predictions])
+    print(f"{fastpropMixresults=}")
